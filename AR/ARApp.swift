405a7a14984bbaceab3c07878e7e7c85d2af3e9a
David <ber.dev@yandex.ru>
2022-12-27 19:53:42 +0300

    Initial Commit

---
+++ AR/ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARApp.swift
+//  AR
+//
+//  Created by Дэвид Бердников on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
